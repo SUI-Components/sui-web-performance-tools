c63f8def57490d70805fdd14b92e597945d8b34d
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-03-19 11:59:47 +0100

    feat(sui-web-performance-metrics): add painting metrics

--- packages/sui-web-performance-metrics/checkHardLoadUrls.js
+++ packages/sui-web-performance-metrics/checkHardLoadUrls.js
@@ -1,15 +1,43 @@
+const { createTimer } = require('./helpers')
 const { withHarResponse } = require('./withHarResponse')
 
+async function getPerformanceMetrics ({ page }) {
+  return page.evaluate(_ => {
+    let paint = {}
+
+    window.performance.getEntriesByType('paint').forEach(entry => {
+      paint[entry.name] = entry.startTime
+    })
+
+    const resources = window.performance.getEntriesByType('resource')
+      .map(({ duration, name }) => ({ duration, name }))
+
+    return { paint, resources }
+  })
+}
+
 async function checkUrl ({ page, url }) {
-  const doOnPage = async () => { await page.goto(url) }
+  const doOnPage = async () => {
+    await page.goto(url)
+    const performanceMetrics = await getPerformanceMetrics({ page })
+    return { performanceMetrics }
+  }
   return withHarResponse(page, doOnPage)
 }
 
+function handleError (err) {
+  console.log(err)
+}
+
 async function checkHardLoadUrls ({ page, hardLoadUrls }) {
+  console.log('· checkHardLoadUrls')
   let checkResults = []
   for (const url of hardLoadUrls) {
-    const singleCheckResult = await checkUrl({ page, url })
-    checkResults.push(singleCheckResult)
+    console.log(`·· checking url ${url}`)
+    const timer = createTimer()
+    const singleCheckResult = await checkUrl({ page, url }).catch(handleError)
+    const timeUsed = timer.stop()
+    checkResults.push({ ...singleCheckResult, timeUsed })
   }
   return checkResults
 }
