63d139b399be280dcb944ce698d2085b985d3062
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-03-19 15:46:41 +0100

    feat(sui-web-performance-metrics): create pageSpeed metric to add

---
+++ packages/sui-web-performance-metrics/checkPageSpeed.js
@@ -0,0 +1,28 @@
+const axios = require('axios')
+
+const EMPTY_RESPONSE = { pageSpeedResult: {} }
+const PAGE_SPEED_URL = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed'
+const STRATEGY_MOBILE = 'mobile'
+const STRATEGY_DESKTOP = 'desktop'
+
+function handleError (err) {
+  console.error(err)
+  return EMPTY_RESPONSE
+}
+
+async function checkPageSpeed ({ googlePageSpeedApiKey, url, viewport }) {
+  const { isMobile } = viewport
+  const strategy = isMobile === true ? STRATEGY_MOBILE : STRATEGY_DESKTOP
+
+  if (typeof googlePageSpeedApiKey === 'undefined') {
+    return EMPTY_RESPONSE
+  }
+
+  const params = { url, strategy }
+  const { data } = await axios.get(PAGE_SPEED_URL, { params }).catch(handleError)
+  return { pageSpeedResult: data }
+}
+
+module.exports = {
+  checkPageSpeed,
+}
